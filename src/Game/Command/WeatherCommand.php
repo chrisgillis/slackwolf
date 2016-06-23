06c04a227c40a11b9f8f1ab592aa18118c7bd370
zkleinfeld <zkleinfeld@gmail.com>
2016-06-23 16:14:44 -0400

    Create WeatherCommand.php

---
+++ src/Game/Command/WeatherCommand.php
@@ -0,0 +1,37 @@
+<?php namespace Slackwolf\Game\Command;
+
+use Exception;
+use Slack\Channel;
+use Slack\ChannelInterface;
+use Slackwolf\Game\Formatter\GameStatusFormatter;
+use Slackwolf\Game\Game;
+
+class WeatherCommand extends Command
+{
+
+    /**
+     * @var Game
+     */
+    private $game;
+
+    public function init()
+    {
+        $this->game = $this->gameManager->getGame($this->channel);
+    }
+
+    public function fire()
+    {
+        $client = $this->client;
+
+        if ( ! $this->gameManager->hasGame($this->channel)) {
+            $client->getChannelGroupOrDMByID($this->channel)
+               ->then(function (ChannelInterface $channel) use ($client) {
+                   $client->send(":warning: Run command this in the game channel.", $channel);
+               });
+            return;
+        }
+
+          $this->gameManager->sendMessageToChannel($this->game, ":rain_cloud: It is raining. It is a cold rain, and the freezing drops chill you to the bone." );
+
+    }
+}
