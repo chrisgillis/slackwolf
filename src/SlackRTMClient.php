ee843565816b627d3cb163cbe00b4abd8d38408d
owena11 <owena11@users.noreply.github.com>
2018-10-29 21:45:08 +0000

    Add in a ping/pong response to check the liveness of the websocket every 5s

--- src/SlackRTMClient.php
+++ src/SlackRTMClient.php
@@ -12,6 +12,12 @@ use Slack\Channel;
 use Slack\Payload;
 use Slack\RealTimeClient;
 
+use GuzzleHttp;
+use React\EventLoop\LoopInterface;
+use React\Promise\Deferred;
+
+use Slackwolf\Message\Message;
+
 /**
  * Class SlackRTMClient
  *
@@ -19,44 +25,53 @@ use Slack\RealTimeClient;
  */
 class SlackRTMClient extends RealTimeClient
 {
-    public function __construct(LoopInterface $loop, GuzzleHttp\ClientInterface $httpClient = null)
+
+    public function connect()
     {
         /*
-         * Call the parent constructor
+         * Call the parent method
          */
-        parent::__construct($loop, $httpClient);
+        $deferred = parent::connect();
 
-        $this->loop->addPeriodicTimer(5, function () {
-            /*
-             * Check if a liveness test has been called before and look the the
-             * result.
-             */
-            if(property_exists($this, 'pong_response')){
-                if($this->pong_response){
-                    echo "Pong Acknowledged...\r\n";
-                } else{
-                    echo "Pong Missing...\r\n";
+        /* Queue our addition of the time onto the promise once its done.
+         */
+        $deferred->then(function() {
+                /*
+                 * If a pong response is returned down the pipes set the flag to true.
+                 */
+                echo "Pong Capture setup...\r\n";
+                $this->on('pong', function ($data) {
+                    $this->pong_response = True;
+                });
+        })->then(function () {
+            $this->loop->addPeriodicTimer(5, function () {
+                /*
+                 * Check if a liveness test has been called before and look the the
+                 * result.
+                 */
+                if(property_exists($this, 'pong_response')){
+                    if($this->pong_response){
+                        echo "Pong Acknowledged...\r\n";
+                    } else{
+                        echo "Pong Missing...\r\n";
+                    }
                 }
-            }
-            /*
-             * Send the ping request down the web socket in order to check the
-             * liveness.
-             */
-            $data = [
-                'id' => ++$this->lastMessageId,
-                'type' => 'ping',
-            ];
-            $this->pong_response = False;
-            $this->websocket->send(json_encode($data));
+                /*
+                 * Send the ping request down the web socket in order to check the
+                 * liveness.
+                 */
+                $data = [
+                    'id' => ++$this->lastMessageId,
+                    'type' => 'ping',
+                ];
+                $this->pong_response = False;
+                $this->websocket->send(json_encode($data));
+                echo "Ping sent...\r\n";
+            });
+
         });
 
-        /*
-         * If a pong response is returned down the pipes set the flag to true.
-         */
-        $this->websocket->on('pong', function ($message) {
-            $this->pong_response = True;
-        });
-
+        return $deferred;
 
     }
     /**
